091236bfca73b6db231608bbfbb2a726a7f202a0
Tomaz Muraus <tomaz@tomaz.me>
2012-12-09 22:03:33 -0800

    Add a script which verify that bundle contains all the necessary files.

---
+++ scripts/verify_code_bundle.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+script=$(readlink -f $0)
+basedir=`dirname $script`
+
+rm -rf ${basedir}/../dist/
+
+python setup.py sdist
+
+cd ${basedir}/../dist/
+tar -xzvf *.tar.gz
+
+# Make sure cacert.pem is included
+if [ ! -f ${basedir}/../dist/service-registry-cli-*/service_registry_cli/data/cacert.pem ]; then
+    echo "cacert.pem doesn't exist"
+    exit 1
+fi
+
+exit 0
