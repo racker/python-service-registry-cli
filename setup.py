625acc5f1b06bfa79c437ac731b115ca1b79d559
Tomaz Muraus <tomaz@tomaz.me>
2013-03-13 12:55:45 -0700

    Try pulling cmd2 in sooner.

--- setup.py
+++ setup.py
@@ -70,12 +70,12 @@ setup(
     },
     platforms=['Any'],
     install_requires=[
-        'cliff >= 1.2.2-dev',
-        'cliff-tablib >= 1.0',
-        'cliff-rackspace >= 0.1.1',
         # newer version is broken in 2.6, see
         # https://bitbucket.org/catherinedevlin/cmd2/issue/3/fix-a-failure-under-python-26
         'cmd2 == 0.6.4',
+        'cliff >= 1.2.2-dev',
+        'cliff-tablib >= 1.0',
+        'cliff-rackspace >= 0.1.1',
         'service-registry >= 0.2.0, < 0.3.0'
     ],
     dependency_links = [
